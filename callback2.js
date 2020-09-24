693d06684d823fcc7157cf5b61362522b3140abd
Stefano Paulo <stefanopaulo47@gmail.com>
2020-09-24 18:40:05 -0300

    funções callback

---
+++ callback2.js
@@ -0,0 +1,16 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+// Sem callback
+const notasBaixas1 = []
+for(let i in notas) {
+    if (notas[i] < 7) notasBaixas1.push(notas[i])
+}
+
+console.log(notasBaixas1)
+
+// Com callback
+const notasMenorQue7 = nota => nota < 7
+
+const notasBaixas2 = notas.filter(notasMenorQue7)
+
+console.log(notasBaixas2)
