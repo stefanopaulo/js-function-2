0baed828ac6fd6c9066f2b849f9e89fcefefe0de
Stefano Paulo <stefanopaulo47@gmail.com>
2020-09-24 20:37:58 -0300

    desafio função construtora

--- desafioFuncaoConstrutora.js
+++ desafioFuncaoConstrutora.js
@@ -1,7 +1,7 @@
 function Pessoa(nome) {
-    this.nome = nome
+    // this.nome = nome
 
-    this.falar = () => console.log(`Meu nome é ${this.nome}`)
+    this.falar = () => console.log(`Meu nome é ${nome}`)
 }
 
 const p1 = new Pessoa('João')
